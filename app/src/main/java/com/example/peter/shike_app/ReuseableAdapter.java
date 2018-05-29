bd66090f64c7af43687ec35c8b0e7dbb0b0a84a5
Luo XuKun <861392914@qq.com>
2018-05-29 22:18:48 +0800

    增加canteenList

---
+++ app/src/main/java/com/example/peter/shike_app/ReuseableAdapter.java
@@ -0,0 +1,10 @@
+package com.example.peter.shike_app;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Administrator on 2018/5/29.
+ */
+
+public abstract class ReuseableAdapter extends BaseAdapter {
+}
