f798c63f0e96e9cc989a16cbba5d8dd262ab77ae
Luo XuKun <861392914@qq.com>
2018-05-31 17:29:36 +0800

    Merge branch 'master' of https://github.com/lzixuan/shike_app
    # Conflicts:
    #	app/src/main/res/layout/fg_content2.xml

--- app/src/main/java/com/example/peter/shike_app/MyFragment2.java
+++ app/src/main/java/com/example/peter/shike_app/MyFragment2.java
@@ -23,6 +23,7 @@ public class MyFragment2 extends Fragment {
     private Button addbtn = null;
     private Context mContext = null;
     private TextView canteen = null;
+    private TextView title = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -39,6 +40,7 @@ public class MyFragment2 extends Fragment {
                     Toast.makeText(getActivity(), "请先登录", Toast.LENGTH_SHORT).show();
             }
         });
+        title = (TextView) view.findViewById(R.id.txt_title);
         canteen = (TextView) view.findViewById(R.id.can);
         registerForContextMenu(canteen);
 
@@ -61,45 +63,59 @@ public class MyFragment2 extends Fragment {
         ListFragment nlFragment = null;
         switch (item.getItemId()) {
             case R.id.xueyi:
+                title.setText("学一食堂");
                 nlFragment = new ListFragment(0, 0);
                 break;
             case R.id.xuewu:
+                title.setText("学五食堂");
                 nlFragment = new ListFragment(0, 1);
                 break;
             case R.id.yiyuan1:
+                title.setText("艺园一楼");
                 nlFragment = new ListFragment(0, 2);
                 break;
             case R.id.yiyuan2:
+                title.setText("艺园二楼");
                 nlFragment = new ListFragment(0, 3);
                 break;
             case R.id.nongyuan1:
+                title.setText("农园一楼");
                 nlFragment = new ListFragment(0, 4);
                 break;
             case R.id.nongyuan2:
+                title.setText("农园二楼");
                 nlFragment = new ListFragment(0, 5);
                 break;
             case R.id.nongyuan3:
+                title.setText("农园三楼");
                 nlFragment = new ListFragment(0, 6);
                 break;
             case R.id.shaoyuan1:
+                title.setText("勺园一楼");
                 nlFragment = new ListFragment(0, 7);
                 break;
             case R.id.shaoyuan2:
+                title.setText("勺园二楼");
                 nlFragment = new ListFragment(0, 8);
                 break;
             case R.id.yannan:
+                title.setText("燕南食堂");
                 nlFragment = new ListFragment(0, 9);
                 break;
             case R.id.tongyuan:
+                title.setText("佟园食堂");
                 nlFragment = new ListFragment(0, 10);
                 break;
             case R.id.changchun:
+                title.setText("畅春园食堂");
                 nlFragment = new ListFragment(0, 11);
                 break;
             case R.id.yixuebu:
+                title.setText("医学部食堂");
                 nlFragment = new ListFragment(0, 12);
                 break;
             case R.id.songlin:
+                title.setText("松林包子");
                 nlFragment = new ListFragment(0, 13);
                 break;
         }
